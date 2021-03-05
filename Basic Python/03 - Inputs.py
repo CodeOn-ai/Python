4ac39812617c6e44771bb78309210f5adf0b8866
CodeOn-ai <76127286+CodeOn-ai@users.noreply.github.com>
2021-03-05 16:58:05 -0300

    Add files via upload

---
+++ Basic Python/03 - Inputs.py
@@ -0,0 +1,13 @@
+# First way
+print("How old are you?", end=' ')
+age = input()
+
+print(f"I'm {age} years old")
+
+# 2nd way
+age = input("How old are you?")
+print(f"I'm {age} years old")
+
+# 3rd way
+age = int(input("Quantos years old você tem? "))
+print(f"I'm {age} years old")
