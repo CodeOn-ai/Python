40e695f70dce754b4968a568e5c87c8c51d0589e
CodeOn-ai <76127286+CodeOn-ai@users.noreply.github.com>
2021-03-05 16:27:22 -0300

    Add files via upload

---
+++ Basic Python/01 -  Comments,Variable and Single Print.py
@@ -0,0 +1,37 @@
+
+# Hello World!
+
+
+# In python you can commet in your code in two ways, using '#' or ... 
+
+"""
+Using triple quotes
+
+Single line comment use #, for a block use the quotes 
+
+
+"""
+# In python you do not need to specify the type like in c++
+
+
+a = 4 # int
+b = 5.4 # float 
+c = True # Boolean
+d = (3 +4j) # complex number
+e = "String" # string
+
+# to print in your interpreter you will need to use the in built function print
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+
+# you can verify the types as well with function type()
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
